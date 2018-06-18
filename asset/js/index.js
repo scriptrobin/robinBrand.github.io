03947ccbcdb3cdc4e08b6ed7d2ec04fd9d970ff0
RobinBrand <rson741@gmail.com>
2018-06-18 21:12:41 +0530

    project created

---
+++ asset/js/index.js
@@ -0,0 +1,20 @@
+console.log('hi')
+/* "version": "0.2.0",
+"configurations": [
+    {
+        "type": "chrome",
+        "request": "attach",
+        "name": "Attach to Chrome",
+        "port": 9222,
+        "webRoot": "${workspaceRoot}",
+        "urlFilter": "http://localhost:8080"
+    },
+    {
+        "type": "chrome",
+        "request": "launch",
+        "name": "Launch Chrome against index.html",
+        "file": "${workspaceRoot}/index.html"
+    }
+] */
+
+asdl
